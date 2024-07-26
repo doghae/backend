35327ee16593e3adbf8f48fca8fa39b5dc0d49bf
LSBsity <dltmdqls51@naver.com>
2024-07-26 22:57:59 +0900

    init

---
+++ src/main/java/team5/doghae/DoghaeApplication.java
@@ -0,0 +1,13 @@
+package team5.doghae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoghaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoghaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team5/doghae/DoghaeApplicationTests.java
@@ -0,0 +1,13 @@
+package team5.doghae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoghaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
