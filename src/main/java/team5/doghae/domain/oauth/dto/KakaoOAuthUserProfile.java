c221e03b115b45f972a328fdc34f93b0a9f3e8eb
LSBsity <dltmdqls51@naver.com>
2024-07-27 23:39:02 +0900

    feat: kakao OAuth user profile dto

--- src/main/java/team5/doghae/domain/oauth/dto/KakaoOAuthUserProfile.java
+++ src/main/java/team5/doghae/domain/oauth/dto/KakaoOAuthUserProfile.java
@@ -1,6 +1,8 @@
 package team5.doghae.domain.oauth.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,10 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Response {
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class KakaoOAuthUserProfile {
 
-    private String email;
+    private KakaoAccount kakaoAccount;
+
 }
+
