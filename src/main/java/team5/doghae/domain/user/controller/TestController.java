431cecd8beab2fad5d15e39f68ca46af6892f94d
LSBsity <dltmdqls51@naver.com>
2024-08-03 11:32:05 +0900

    Feat: CICD Test Controller

---
+++ src/main/java/team5/doghae/domain/user/controller/TestController.java
@@ -0,0 +1,14 @@
+package team5.doghae.domain.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "hi hi hi !";
+    }
+
+}
