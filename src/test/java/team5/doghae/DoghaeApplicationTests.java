37132bc51e8bc29d73dd3135c6a0aeddcc1bc405
LSBsity <dltmdqls51@naver.com>
2024-08-03 13:22:55 +0900

    chore: test

--- src/test/java/team5/doghae/DoghaeApplicationTests.java
+++ src/test/java/team5/doghae/DoghaeApplicationTests.java
@@ -3,7 +3,7 @@ package team5.doghae;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest(classes = DoghaeApplicationTests.class)
 class DoghaeApplicationTests {
 
     @Test
