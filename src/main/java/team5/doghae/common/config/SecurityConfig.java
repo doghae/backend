da53df2380efe474a7f54e71943da4f904c5ae58
LSBsity <dltmdqls51@naver.com>
2024-08-05 13:56:35 +0900

    Fix: headers

--- src/main/java/team5/doghae/common/config/SecurityConfig.java
+++ src/main/java/team5/doghae/common/config/SecurityConfig.java
@@ -84,11 +84,11 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
         configuration.addAllowedOrigin("https://doghae.vercel.app/");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With", "Accept", "Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         configuration.setExposedHeaders(Arrays.asList("*"));
-        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
