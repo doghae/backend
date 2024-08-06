465d1019dcf21cdfc9fc22e701bb4113c3bc3cee
LSBsity <dltmdqls51@naver.com>
2024-08-06 13:36:45 +0900

    Refactor: Return Type 명시

--- src/main/java/team5/doghae/domain/review/ReviewController.java
+++ src/main/java/team5/doghae/domain/review/ReviewController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 import team5.doghae.common.resolver.AuthUser;
 import team5.doghae.common.response.SuccessResponse;
 import team5.doghae.common.security.jwt.JwtTokenInfo;
+import team5.doghae.domain.review.dto.ReviewResponse;
 import team5.doghae.domain.review.service.ReviewService;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,7 +20,7 @@ public class ReviewController {
     private final ReviewService reviewService;
 
     @GetMapping("/reviews")
-    public ResponseEntity<?> getWrongQuestion(
+    public ResponseEntity<SuccessResponse<List<ReviewResponse.WrongAnswer>>> getWrongQuestion(
             @AuthUser JwtTokenInfo jwtTokenInfo
     ) {
         return SuccessResponse.of(
