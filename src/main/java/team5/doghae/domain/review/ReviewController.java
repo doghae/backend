deaf49a17aded75919922776c742c2c8c4d067a9
LSBsity <dltmdqls51@naver.com>
2024-08-06 13:12:16 +0900

    Feat: 틀린 문제들 키워드 별로 정렬하여 반환

---
+++ src/main/java/team5/doghae/domain/review/ReviewController.java
@@ -0,0 +1,28 @@
+package team5.doghae.domain.review;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team5.doghae.common.resolver.AuthUser;
+import team5.doghae.common.response.SuccessResponse;
+import team5.doghae.common.security.jwt.JwtTokenInfo;
+import team5.doghae.domain.review.service.ReviewService;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @GetMapping("/reviews")
+    public ResponseEntity<?> getWrongQuestion(
+            @AuthUser JwtTokenInfo jwtTokenInfo
+    ) {
+        return SuccessResponse.of(
+                reviewService.getWrongQuestions(jwtTokenInfo.getUserId())
+        ).setStatus(HttpStatus.OK);
+    }
+
+}
--- src/main/java/team5/doghae/domain/review_question_map/repository/ReviewQuestionRepository.java
+++ src/main/java/team5/doghae/domain/review_question_map/repository/ReviewQuestionRepository.java
@@ -1,7 +1,15 @@
 package team5.doghae.domain.review_question_map.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team5.doghae.domain.question.domain.Question;
+import team5.doghae.domain.review.domain.Review;
 import team5.doghae.domain.review_question_map.domain.ReviewQuestionMap;
 
+import java.util.List;
+
 public interface ReviewQuestionRepository extends JpaRepository<ReviewQuestionMap, Long> {
+
+    List<ReviewQuestionMap> findAllByReview(Review review);
+
+    List<ReviewQuestionMap> findAllByReviewAndQuestion(Review review, Question question);
 }
